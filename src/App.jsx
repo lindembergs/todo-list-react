1998ef9645c768c0897b0ef4526a3296f50e3d7a
lindembergs <lindembergparaiba@gmail.com>
2023-11-01 18:17:41 -0300

    implementação do map e useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-// import { useState } from 'react'
-
+import  { useState } from 'react';
 
 function App() {
- 
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
 
   return (
-    <div>olá brasil</div>
-  )
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="content">
+              {todo.text}
+              <p className='category'>{todo.category}</p>
+              
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
