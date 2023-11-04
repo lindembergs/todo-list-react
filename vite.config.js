02f438834413aeefcd62dafc9a38f711ece03de7
lindembergs <lindembergparaiba@gmail.com>
2023-11-04 15:56:30 -0300

    tentativa de deploy no gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,36 +6,41 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      name: "Weather Ups",
-      short_name: "Weather Ups",
-      description: "An app that shows weather forecasts for your city.",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#e8ebf2",
-      theme_color: "#171717",
-      icons: [
-        {
-          src: "./src/icons/logoapp.jpg",
-          sizes: "192x192",
-          type: "image/jpg",
-        },
-        {
-          src: "./src/icons/logoapp.jpg",
-          sizes: "512x512",
-          type: "image/jpg",
-        },
-        {
-          src: "./src/icons/logoapp.jpg",
-          sizes: "180x180",
-          type: "image/jpg",
-        },
-        {
-          src: "./src/icons/logoapp.jpg",
-          sizes: "225x225",
-          type: "image/jpg",
-          purpose: "maskable",
-        },
-      ],
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "React Vite App",
+        short_name: "Vite App",
+        description: "I am a simple Vite app",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#171717",
+        background_color: "#f0e7db",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
     }),
   ],
+  // Configuração da opção 'base'
+  base: "/todo-list-react", // Substitua pelo caminho da sua escolha
 });
