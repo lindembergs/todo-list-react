1e5f731052276c94c20f00c758c6910278f11cd0
lindembergs <lindembergparaiba@gmail.com>
2023-11-04 14:14:09 -0300

    tentativa de add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import VitePWA from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-
-      manifest: {
-        name: "Lista de tarefas",
-        short_name: "Lista de tarefas",
-        description:
-          "Aplicação para lembrar da lista de tarefas e saber as tarefas que já foram feitas.",
-        theme_color: "#000000",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "/path/to/icon.png", // Substitua pelo caminho correto para o ícone
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-      },
+      name: "Weather Ups",
+      short_name: "Weather Ups",
+      description: "An app that shows weather forecasts for your city.",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#e8ebf2",
+      theme_color: "#171717",
+      icons: [
+        {
+          src: "./src/icons/logoapp.jpg",
+          sizes: "192x192",
+          type: "image/jpg",
+        },
+        {
+          src: "./src/icons/logoapp.jpg",
+          sizes: "512x512",
+          type: "image/jpg",
+        },
+        {
+          src: "./src/icons/logoapp.jpg",
+          sizes: "180x180",
+          type: "image/jpg",
+        },
+        {
+          src: "./src/icons/logoapp.jpg",
+          sizes: "225x225",
+          type: "image/jpg",
+          purpose: "maskable",
+        },
+      ],
     }),
   ],
 });
